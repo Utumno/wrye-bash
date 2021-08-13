ad7f0f648998418a87f099d7d0496ffaa25f8905
MrD <the.ubik@gmail.com>
2021-08-13 16:46:15 +0300

    TTT release mouse traceback on hitting restart
    Traceback (most recent call last):
      File "/Users/ubik-mac13/Dropbox (Personal)/eclipse_workspaces/python/wrye-bash/Mopy/bash/bash.py", line 273, in main
        _main(opts, wx_locale)
      File "/Users/ubik-mac13/Dropbox (Personal)/eclipse_workspaces/python/wrye-bash/Mopy/bash/bash.py", line 452, in _main
        app.MainLoop()
      File "/Users/ubik-mac13/Dropbox (Personal)/eclipse_workspaces/python/wrye-bash/venv/venv/lib/python3.9/site-packages/wx/core.py", line 2237, in MainLoop
        rv = wx.PyApp.MainLoop(self)
    wx._core.wxAssertionError: C++ assertion "!wxMouseCapture::IsInCaptureStack(this)" failed at /Users/robind/projects/bb2/dist-osx-py39/build/ext/wxWidgets/src/common/wincmn.cpp(453) in ~wxWindowBase(): Destroying window before releasing mouse capture: this will result in a crash later.
    
    Turns out it's the balt.DnDStatusBar.OnDragEnd (duh) - on calling
    event.Skip() - dragging started when user clicks on  restart button  -
    then when destroying the Status bar realizes  mouse was never released.
    Made it always release the mouse if captured TTT

--- Mopy/bash/balt.py
+++ Mopy/bash/balt.py
@@ -2243,11 +2243,10 @@ class DnDStatusBar(wx.StatusBar):
     def OnDragEnd(self, event):
         if self.dragging != wx.NOT_FOUND:
             try:
-                if self.moved:
-                    for button in self.buttons:
-                        if button._native_widget.HasCapture():
-                            button._native_widget.ReleaseMouse()
-                            break
+                for button in self.buttons:
+                    if button._native_widget.HasCapture():
+                        button._native_widget.ReleaseMouse()
+                        break
             except:
                 # deprint(u'Exception while handling mouse up on button',
                 #         traceback=True)
